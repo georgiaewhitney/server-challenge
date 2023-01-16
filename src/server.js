595e434b08fcd8b0838383fe931b1ef38021f4e5
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-01-16 02:45:15 +0000

    challenge 1 - passed

--- src/server.js
+++ src/server.js
@@ -3,3 +3,21 @@ const express = require("express");
 const server = express();
 
 module.exports = server;
+
+//challenge 1
+server.get("/", (request, response) => {
+  response.send(
+    `
+    <!doctype html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+      </head>
+      <body>
+        <h1>Hello Express</h1>
+      </body>
+    </html>
+    `
+  );
+});
